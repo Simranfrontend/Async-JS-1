da4b7a89eb33eee495d89e43a52ef1417f3199e1
Simran <simran.guptalkg2409@gmail.com>
2025-11-28 22:36:19 +0530

    Added callback2 assignment

---
+++ callback2.js
@@ -0,0 +1,13 @@
+
+function timer(duration, onComplete) {
+    console.log(`Timer started for ${duration} ms...`);
+
+    setTimeout(() => {
+       
+        onComplete(`Timer of ${duration} ms finished`);
+    }, duration);
+}
+
+timer(2000, (message) => {
+    console.log(message);
+});
